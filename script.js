bac4deb96baa2ce8932ddddc95f88a3bebbb452a
nlawler1737 <72905287+nlawler1737@users.noreply.github.com>
2022-03-08 17:40:29 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let style = document.createElement('style')
+style.innerHTML = `
+html {
+    overflow: hidden
+}
+.hideGrid {
+    grid-area: auto !important;
+}
+.hide {
+    display: none !important;
+}
+#editor > div.tabsContainer.tabsH {
+    width: 100vw !important
+}
+`
+document.head.appendChild(style)
+
+
+let d = (e)=>{return document.querySelector(e)}
+let layout = ()=>document.querySelector("#layout > label:nth-child(7)").click()
+d('#app-updates').classList.add('hide')
+d("#collaborate").innerHTML = d("#collaborate").innerHTML.replaceAll('Collaborate', 'Collab')
+d("#sidebar").classList.add('hide')
+d("#content").classList.add('hideGrid')
+const removeText = ()=>{document.querySelectorAll('.aiButton').forEach(e=>{e.onclick = removeText; e.innerHTML = e.innerHTML.replace(new RegExp(`${e.innerText}([^${e.innerText}]*)$`), '$1')})}
+const showSidebar = ()=>{
+    let a = d("#content").classList.toggle('hideGrid')
+    let b = d('#sidebar').classList.toggle('hide')
+}
+removeText()
+layout()
